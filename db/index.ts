897c9583be8472f841537660fb30b2e58f86c1d5
Manuel Patrone <po75558@gmail.com>
2026-09-14 17:29:52 +0100

    insert_property

--- db/index.ts
+++ db/index.ts
@@ -1,5 +1,8 @@
-import { drizzle } from 'drizzle-orm/neon-http';
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
+import * as schema from "./schema";
 
-const db = drizzle(process.env.DATABASE_URL!);
+const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle({ client: sql }, { schema });
 
-export { db };
+
