2850c96691d04a7ea9080e35b35e94f365d56ab1
Manuel Patrone <po75558@gmail.com>
2026-09-14 17:44:20 +0100

    drizzle problems

--- db/index.ts
+++ db/index.ts
@@ -3,6 +3,7 @@ import { neon } from "@neondatabase/serverless";
 import * as schema from "./schema";
 
 const sql = neon(process.env.DATABASE_URL!);
-export const db = drizzle({ client: sql }, { schema });
+// export const db = drizzle({ client: sql }, { schema });
+export const db = drizzle(sql, { schema });
 
 
