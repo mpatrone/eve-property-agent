6ccb8448f4a264ef31f67d139b659dd0e91c10b9
Manuel Patrone <po75558@gmail.com>
2026-09-14 18:57:46 +0100

    drizzle problem cont1

--- db/index.ts
+++ db/index.ts
@@ -1,9 +1,9 @@
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
-import * as schema from "./schema";
+import * as schema from "./schema.ts";
 
 const sql = neon(process.env.DATABASE_URL!);
-// export const db = drizzle({ client: sql }, { schema });
 export const db = drizzle(sql, { schema });
+
 
 
